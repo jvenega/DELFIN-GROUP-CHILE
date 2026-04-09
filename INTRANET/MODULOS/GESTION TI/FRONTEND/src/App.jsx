e85ebd0aed1818b179a4c1f9d2e5c367781b99c9
Juan Venegas <juanvenegasfuenzalida@gmail.com>
2026-04-09 11:19:48 -0400

    fixed: h-screen

--- INTRANET/MODULOS/GESTION TI/FRONTEND/src/App.jsx
+++ INTRANET/MODULOS/GESTION TI/FRONTEND/src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./layouts/Navbar";
+
 import Home from "./pages/Home";
 import Estados from "./pages/Estados";
 import Marcas from "./pages/Marcas";
@@ -9,26 +10,31 @@ import Tipos from "./pages/Tipos";
 import Movimientos from "./pages/Movimientos";
 import ArticulosPorPersona from "./pages/ArticulosPorPersona";
 
-
-
-// App principal
 export default function App() {
   return (
     <Router basename="/intranet/gestion-ti">
-      <Navbar />
+      
+      <div className="h-screen flex flex-col bg-gray-50">
 
-      <div className="bg-white max-h-screen overflow-auto pt-16">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/estados" element={<Estados />} />
-          <Route path="/marcas" element={<Marcas />} />
-          <Route path="/modelo" element={<Modelo />} />
-          <Route path="/proveedores" element={<Proveedores />} />
-          <Route path="/tipos" element={<Tipos />} />
-          <Route path="/movimientos" element={<Movimientos />} />
-          <Route path="/articulos" element={<ArticulosPorPersona />} />
-        </Routes>
+        {/* NAVBAR */}
+        <Navbar />
+
+        {/* CONTENIDO */}
+        <main className="flex-1 overflow-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/estados" element={<Estados />} />
+            <Route path="/marcas" element={<Marcas />} />
+            <Route path="/modelo" element={<Modelo />} />
+            <Route path="/proveedores" element={<Proveedores />} />
+            <Route path="/tipos" element={<Tipos />} />
+            <Route path="/movimientos" element={<Movimientos />} />
+            <Route path="/articulos" element={<ArticulosPorPersona />} />
+          </Routes>
+        </main>
+
       </div>
+
     </Router>
   );
 }
